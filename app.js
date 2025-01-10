7e48fa3481fff99641bf82da9db8c7783d5bb681
Bakhtiar Rasheed <bakhtiar.rasheed56@gmail.com>
2025-01-10 23:09:09 +0300

    remove duplicated and redundant code

--- app.js
+++ app.js
@@ -1,56 +1,114 @@
 const app = Vue.createApp({
-    data() {
-        return {
-          newTask: '',
-          tasks: [],
-          selectedFilter:'all',
-          filteredTasks:[]
-        };
-      },
-      mounted() {
-      this.tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  data() {
+    return {
+      newTask: '',
+      tasks: [],
+      selectedFilter: 'all',
+      filteredTasks: []
+    };
+  },
+  mounted() {
+    this.tasks = this.loadFromLocalStorage()
+    this.filteredTasks = this.tasks
+  },
+  methods: {
+    loadFromLocalStorage() {
+      try {
+        return JSON.parse(localStorage.getItem('tasks')) || [];
+
+      }
+      catch (err) {
+        console.log("Failed to load from localStorage")
+      }
     },
-     methods: {
-        addTask() {
-          if (this.newTask.trim() !== '') {
-            this.tasks.push({ text: this.newTask, completed: false, isEditing: false });
-            this.newTask = '';
-            localStorage.setItem('tasks', JSON.stringify(this.tasks));
-          }
-        },
-        removeTask(index) {
-          this.tasks.splice(index, 1);
-          localStorage.setItem('tasks', JSON.stringify(this.tasks));
-        },
-        toggleComplete(index,completed){
-         this.tasks[index].completed=completed
-          localStorage.setItem('tasks', JSON.stringify(this.tasks));
-    
-    
-    
-        },
-        toggleFilter(filterValue){
-          console.log(filterValue)
-          if(filterValue==="completed"){
-            this.filteredTasks=this.tasks.filter((item)=>item.completed)
-          }
-          else if(filterValue==="uncompleted"){
-            this.filteredTasks=this.tasks.filter((item)=>!item.completed)
-    
-    
-          }
+    saveToLocalStorage() {
+      try {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+
+      }
+      catch (err) {
+        console.log("Failed to save to localStorage")
+      }
+    },
+
+    addTask() {
+      try {
+        if (this.newTask.trim() !== '') {
+          this.tasks.push({ text: this.newTask, completed: false, isEditing: false });
+          this.newTask = '';
+          this.saveToLocalStorage()
         }
-        ,
-        editTask(task) {
-          task.isEditing = true;
-        },
-        saveTask(task, index) {
-          task.isEditing = false;
-          let newTask={text: task.text, completed: task.completed, isEditing: task.isEditing }
-          this.tasks[index]=newTask
-          localStorage.setItem('tasks', JSON.stringify(this.tasks));
+
+      }
+      catch (err) {
+        console.log("Failed to add task")
+      }
+
+    },
+    removeTask(index) {
+      try {
+        this.tasks.splice(index, 1);
+        this.saveToLocalStorage()
+
+      }
+      catch (err) {
+        console.log("Failed to remove task")
+      }
+
+
+
+    },
+    toggleComplete(index, completed) {
+      try {
+        this.tasks[index].completed = completed
+        this.saveToLocalStorage()
+
+      }
+      catch (err) {
+        console.log("Failed to toggle checkbox")
+      }
+    },
+    toggleFilter(filterValue) {
+
+      try {
+        if (filterValue === "completed") {
+          this.filteredTasks = this.tasks.filter((item) => item.completed)
+        }
+        else if (filterValue === "uncompleted") {
+          this.filteredTasks = this.tasks.filter((item) => !item.completed) 
+        }
+        else {
+          this.filteredTasks = this.tasks
+
         }
       }
+      catch (err) {
+        console.log("Failed to toggle filter")
+      }
+
+    }
     ,
+    editTask(task) {
+      try {
+        task.isEditing = true;
+
+      }
+      catch (err) {
+        console.log("Failed to edit task")
+      }
+    },
+    saveTask(task, index) {
+      try {
+        task.isEditing = false;
+        let newTask = { text: task.text, completed: task.completed, isEditing: task.isEditing }
+        this.tasks[index] = newTask
+        this.saveToLocalStorage()
+      }
+      catch (err) {
+        console.log("Failed to save task")
+      }
+    }
+
+  }
+  ,
 })
-    
